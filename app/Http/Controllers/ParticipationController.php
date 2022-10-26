80095d50fe95538803696bdf04bd3942cd4584b1
gang <67897876+miseliongang@users.noreply.github.com>
2022-10-26 21:00:00 +0330

    add participation model

---
+++ app/Http/Controllers/ParticipationController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Participation;
+use App\Http\Requests\StoreParticipationRequest;
+use App\Http\Requests\UpdateParticipationRequest;
+
+class ParticipationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreParticipationRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreParticipationRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Participation  $participation
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Participation $participation)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Participation  $participation
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Participation $participation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateParticipationRequest  $request
+     * @param  \App\Models\Participation  $participation
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateParticipationRequest $request, Participation $participation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Participation  $participation
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Participation $participation)
+    {
+        //
+    }
+}
